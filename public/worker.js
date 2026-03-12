4c58dca4c826730d99735d495852876926675702
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2026-03-12 22:37:21 +0530

    updated worker.js notification click redirect 2

--- public/worker.js
+++ public/worker.js
@@ -2,9 +2,11 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
+
 
 self.addEventListener("push", event => {
 
@@ -12,20 +14,25 @@ self.addEventListener("push", event => {
 
   let data = {
     title: "Notification",
-    body: "Push notification working"
+    body: "Push notification working",
+    url: "http://localhost:5173"
   };
 
   if (event.data) {
     try {
       data = event.data.json();
-    } catch {
+    } catch (err) {
       data.body = event.data.text();
     }
   }
 
   const options = {
     body: data.body,
-    tag: Date.now().toString() // unique tag for every notification
+    icon: "/logo192.png",
+    tag: Date.now().toString(), // unique notification
+    data: {
+      url: data.url || "http://localhost:5173"
+    }
   };
 
   event.waitUntil(
@@ -39,27 +46,27 @@ self.addEventListener("notificationclick", event => {
 
   event.notification.close();
 
-  const url = event.notification.data.url;
+  const url = event.notification.data?.url || "https://push-notification-frontend-inky.vercel.app/";
 
   event.waitUntil(
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    }).then(clientList => {
 
-    clients.matchAll({ type: "window", includeUncontrolled: true })
-      .then(clientList => {
+      for (const client of clientList) {
 
-        for (const client of clientList) {
-
-          if (client.url === url && "focus" in client) {
-            return client.focus();
-          }
-
+        if (client.url === url && "focus" in client) {
+          return client.focus();
         }
 
-        if (clients.openWindow) {
-          return clients.openWindow(url);
-        }
+      }
 
-      })
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
 
+    })
   );
 
 });
