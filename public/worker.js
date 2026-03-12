543b91a5ba1ef8e67207aa4116f5d45d8f14c1ce
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2026-03-12 22:31:48 +0530

    updated worker.js notification click redirect

--- public/worker.js
+++ public/worker.js
@@ -33,3 +33,33 @@ self.addEventListener("push", event => {
   );
 
 });
+
+
+self.addEventListener("notificationclick", event => {
+
+  event.notification.close();
+
+  const url = event.notification.data.url;
+
+  event.waitUntil(
+
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then(clientList => {
+
+        for (const client of clientList) {
+
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+
+      })
+
+  );
+
+});
